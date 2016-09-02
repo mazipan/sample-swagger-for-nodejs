b0ca0acc058511812d2ea197e1161a9dd68b0726
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2016-09-02 09:17:43 +0700

    add index with jade

--- app.js
+++ app.js
@@ -3,13 +3,43 @@
  */
 'use strict';
 
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+
+var express = require('express');
 var SwaggerExpress = require('swagger-express-mw');
 var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
-var app = require('express')();
+
+var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
 
 // include api product file
 var products = require('./api/controllers/products');
+app.use('/api/products', products);
 
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Sample Using Swagger by Irfan Maulana'
+  });
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 module.exports = app; // for testing
 
@@ -17,12 +47,10 @@ var config = {
   appRoot: __dirname // required config
 };
 
-
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
 
-  app.use('/api/products', products);
   // Add swagger-ui (This must be before swaggerExpress.register)
   app.use(SwaggerUi(swaggerExpress.runner.swagger));
 
